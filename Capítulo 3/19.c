3187a15383371cdd9389988df978f970e50fd8bb
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-08-30 21:34:33 -0300

    Questão 19

---
+++ Capítulo 3/19.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+
+int fatorial(int n);
+
+void main(void){
+    int r, n = 0;
+    setlocale(LC_ALL, "Portuguese");
+    scanf("%d%d", &r, &n);
+    float ncr = fatorial(n) / fatorial(r) * fatorial(n - r);
+    printf("%.2f\n",ncr);
+    system("pause");
+}
+
+int fatorial(int n){
+    int fatorial = 1;
+    for(int i = n; i > 1; i--){
+        fatorial *= i;
+    }
+    return fatorial;
+}
