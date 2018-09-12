ae374960f2cfe206af0f12c74ad98fea72f47620
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-09-11 23:01:01 -0300

    Questão 39

--- Capítulo 5/39.c
+++ Capítulo 5/39.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void a(int *x);
+int primo(int n)
+{
+int i;
 
-void main(void){
-    int d = 2;
-    printf("D: %d\n", d);
-    a(&d);
-    printf("D: %d\n", d);
-    system("pause");
+for (i = 2; i < n; i++)
+{
+if (n % i == 0)
+	return 0;
+i++;
 }
 
-void a(int *x){
-    *x = *x * *x;
+return 1;
+}
+
+void main(void)
+{
+int a, i, j=0,k;
+
+do
+{
+printf("Introduza um numero par superior a 4: ");
+scanf("%d", &a);
+}while((a<=4) || (a%2!=0)) ; 
+
+for(k=4; k<=a ; k=k+2)
+{
+for(i=1 ; i<=k/2 ; i++)
+{
+	if(primo(i))
+		if(primo(k-i))
+			j=k-i;
+}
+printf("O numero %d, e a soma dos primos %d + %d\n", k, k-j, j);
+}
+
+system("PAUSE");
 }
