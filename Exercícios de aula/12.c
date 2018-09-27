8f089a50010541b61c6bb5dd0764be6c29ab343c
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-09-27 10:55:13 -0300

    Questão 12

---
+++ Exercícios de aula/12.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void){
+    union coin {
+        float real, dolar;
+    };
+    typedef struct{
+        char title[50];
+        char author[50];
+        int registration;
+        union coin price;
+    } book;
+    book books[10];
+    int moeda;
+    for(int k = 0; k < 10; k++){
+        printf("Informe o nome do livro: ");
+        gets(books[k].title);
+        printf("Informe o nome do autor: ");
+        gets(books[k].author);
+        printf("Informe o registro do livro: ");
+        scanf("%d", &books[k].registration);
+        printf("Escolha a moeda do preco do livro:\n ");
+        printf("1 - Dolar\n");
+        printf("2 - Real\n>>> ");
+        scanf("%d", &moeda);
+        printf("Informe o preco do livro : ");
+        switch(moeda){
+            case 1:
+                scanf("%f", &books[k].price.dolar);
+                break;
+            case 2:
+                scanf("%d", &books[k].price.real);
+                break;
+            default:
+                printf("\nEscolha invalida !\n");
+        }
+    }
+    system("pause");
+}
